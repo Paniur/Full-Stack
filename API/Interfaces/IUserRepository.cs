994787d93152f2cb10b3af34b8f8d784260c39ce
Delyan <delyan.stoichkov@gmail.com>
2024-02-28 15:21:26 +0200

    Section 8 Queries, AutoMapper, Extending the API

---
+++ API/Interfaces/IUserRepository.cs
@@ -0,0 +1,18 @@
+﻿using API.DTOs;
+using API.Entities;
+
+namespace API.Interfaces
+{
+    public interface IUserRepository
+    {
+        void Update(AppUser user);
+        Task<bool> SaveAllAsync();
+        Task<IEnumerable<AppUser>> GetUsersAsync();
+        Task<AppUser> GetUseByIdAsync(int id);
+        Task<AppUser> GetUserByUsernameAsync(string username);
+        Task<IEnumerable<MemberDto>> GetMembersAsync();
+        Task<MemberDto> GetMemberByUsernameAsync(string username);
+    }
+}
+
+
